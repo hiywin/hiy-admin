d8f1f65816f63242b363261f8611571932428ce1
hiywin <hellowin1853@gmail.com>
2020-01-01 16:47:40 +0800

    配置全局样式

---
+++ src/views/Login/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="login">
+    {{ msg }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "登陆"
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
